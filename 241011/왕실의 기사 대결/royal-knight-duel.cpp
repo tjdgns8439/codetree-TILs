9c0f9286ae5c2d2cfdbb38079c8e5fb4ada7782c
tjdgns8439 <100510247+tjdgns8439@users.noreply.github.com>
2024-10-11 17:46:35 +0900

    Title: 왕실의 기사 대결, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241011/왕실의 기사 대결/royal-knight-duel.cpp
+++ 241011/왕실의 기사 대결/royal-knight-duel.cpp
@@ -18,12 +18,16 @@ int dx[4] = {-1,0,1,0};
 int dy[4] = {0,1,0,-1};
 int chess[51][51] = {0,};// 체스판 정보
 int personMap[51][51] = {0,}; //사람 정보
-
+int firstPersonK[51] = {0,}; //사람 정보
+int result = 0;
 
 bool isRange(int i, int j){
     return i>=0 & j>=0 && i<L && j<L && chess[i][j] != 2;
 }
+int curIdx = 0;
+vector<int> v;
 void move(int idx, int d){
+    if(idx != curIdx) v.push_back(idx);
     PERSON* curPerson = &personList[idx];
     int temp[51][51] = {0,};
     for(int i = curPerson->x; i < curPerson->x + curPerson->h; i++){
@@ -34,13 +38,13 @@ void move(int idx, int d){
             personMap[i][j] = 0;
         }
     }
-    pair<int,int> start;
+    pair<int,int> start={0,0};
     for(int i = curPerson->x; i < curPerson->x + curPerson->h; i++){
         for(int j = curPerson->y; j < curPerson->y + curPerson->w; j++ ){
             int nx = dx[d] + i;
             int ny = dy[d] + j;
             personMap[nx][ny] = temp[nx][ny];
-            if(nx == curPerson->x && ny == curPerson->y){
+            if(i == curPerson->x && j == curPerson->y){
                 start = {nx,ny};
             }
         }
@@ -68,6 +72,25 @@ bool can_move(int idx, int d){ //내려가면서 마주친 애들 모두
     return true;
 }
 
+void deletePerson(int idx){
+    int cnt =0;
+    PERSON* person = &personList[idx];
+    for(int x = person->x; x<person->x+person->h; x++){
+        for(int y = person->y; y<person->y + person->w; y++){
+            if(chess[x][y] == 1) cnt++;
+        }
+    }
+    person->k -= cnt;
+    if(person->k < 0){
+        person->k = 0;
+        for(int x = person->x; x<person->x+person->h; x++){
+            for(int y = person->y; y<person->y + person->w; y++){
+                personMap[x][y] = 0;
+            }
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin>>L>>N>>Q;
@@ -76,10 +99,12 @@ int main() {
             cin>>chess[i][j];
         }
     }
+
     for(int i =1; i<=N; i++){
         int r,c,h,w,k;
         cin>>r>>c>>h>>w>>k;
         personList[i] = {r-1,c-1,h,w,k};
+        firstPersonK[i] = k;
     }
     //set person map
     for(int i =1; i<=N; i++){
@@ -91,36 +116,26 @@ int main() {
         }
     }
 
-    int result = 0;
-
     for( int i =0; i<Q; i++){
         int idx,d;
         cin>>idx>>d;
-
-//        for(int q =0; q<L; q++){
-//            for(int j =0; j<L; j++){
-//                cout<<personMap[q][j]<<" ";
-//            }
-//            cout<<endl;
-//        }
-//        cout<<endl<<endl;
-
+        curIdx = idx;
         if(personList[idx].k <= 0) continue;
-
         if(!can_move(idx,d)) continue;
-
-        for(int q =0; q<L; q++){
-            for(int j =0; j<L; j++){
-                if(personMap[q][j] == idx) continue;
-                if(chess[q][j] == 1){
-                    if(personMap[q][j] != 0){
-                        personList[personMap[q][j]].k--;
-                        result++;
-                    }
-                }
-            }
+        for(int j =0; j<v.size(); j++){
+            deletePerson(v[j]);
         }
+        v.clear();
+
     }
+
+    for(int i =1; i<=N; i++){
+        if(personList[i].k <= 0 ) continue;
+        result = result + firstPersonK[i] - personList[i].k;
+    }
+
+
+
 
     cout<<result;
 
